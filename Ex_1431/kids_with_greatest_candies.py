33ba16b88b242d1e82a6f2e00b73ab0a303c52d5
Harshil Patel <harshilpatel1903@gmail.com>
2021-03-28 10:28:45 +0530

    1431. Kids With the Greatest Number of Candies
    Source : https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/

---
+++ Ex_1431/kids_with_greatest_candies.py
@@ -0,0 +1,16 @@
+class Solution:
+    def kidsWithCandies(self, candies, extraCandies):
+        l = []
+        for i in candies:
+            if ((i + extraCandies) >= max(candies)):
+                l.append(True)
+            else:
+                l.append(False)
+
+        return l
+
+if __name__ == '__main__':
+    s = Solution();
+
+    print(s.kidsWithCandies([2,3,5,1,3], 3))
+    print(s.kidsWithCandies([4,2,1,1,2], 1))
