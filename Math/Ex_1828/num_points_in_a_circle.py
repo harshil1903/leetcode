e747f7588a10a1b2b996e6afa11e999ce2f4b509
Harshil Patel <harshilpatel1903@gmail.com>
2021-05-03 11:59:03 +0530

    1828. Queries on Number of Points Inside a Circle
    Source : https://leetcode.com/problems/queries-on-number-of-points-inside-a-circle/

--- Math/Ex_1828/num_points_in_a_circle.py
+++ Math/Ex_1828/num_points_in_a_circle.py
@@ -3,26 +3,13 @@ from typing import List
 
 class Solution:
     def countPoints(self, points: List[List[int]], queries: List[List[int]]) -> List[int]:
-        ans = [0] * len(queries)
-        i = 0;
-        print(ans)
-        for q in queries:
-            for p in points:
-
-                if((q[0] - q[2]) <= p[0] <= (q[0] + q[2])):
-                    if((q[1] - q[2]) <= p[1] <= (q[1] + q[2])):
-
-                        if(p[0] == q[0] - q[2] or p[0] == q[0] + q[2]):
-                            if(p[1] != q[1] - q[2] and p[1] != q[1] + q[2]):
-                                ans[i] += 1
-                        elif (p[1] == q[1] - q[2] or p[1] == q[1] + q[2]):
-                            if (p[0] != q[0] - q[2] and p[0] != q[0] + q[2]):
-                                ans[i] += 1
-                        else:
-                            ans[i] += 1
-            i+=1
-
-        return ans;
+        result = []
+        for x1, y1, r in queries:
+            result.append(0)
+            for x, y in points:
+                if (x - x1) ** 2 + (y - y1) ** 2 <= r ** 2:
+                    result[-1] += 1
+        return result
 
 if __name__ == '__main__':
     s = Solution()
