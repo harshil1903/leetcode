3723efb1c214fb4a96892b7b270078147cbeaec3
Harshil Patel <harshilpatel1903@gmail.com>
2021-05-08 12:35:56 +0530

    414. Third Maximum Number
    Source : https://leetcode.com/problems/third-maximum-number/

--- Array/Ex_0414/third_maximum_number.py
+++ Array/Ex_0414/third_maximum_number.py
@@ -11,8 +11,28 @@ class Solution:
         s.remove(max(s))
         return max(s)
 
+    def thirdMax1(self, nums: List[int]) -> int:
+        nums = sorted(set(nums), reverse=True)
+        if len(nums) < 3:
+            return nums[0]
+        else:
+            return nums[2]
+
+    def thirdMax2(self, nums: List[int]) -> int:
+        a = b = c = float('-inf')
+        for i in nums:
+            if i > a:
+                c, b, a = b, a, i
+            elif i < a and i > b:
+                c, b = b, i
+            elif i < b and i > c:
+                c = i
+
+        if c == float('-inf'):
+            return a
+        return c
 if __name__ == "__main__":
     s = Solution()
     print(s.thirdMax([3,2,1]))
-    print(s.thirdMax([2,2,3,1]))
-    print(s.thirdMax([1,2]))
+    print(s.thirdMax1([2,2,3,1]))
+    print(s.thirdMax2([1,2]))
