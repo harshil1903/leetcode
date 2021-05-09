09f6f020cd7e5ce47fb21ff2703f2158da7386a9
Harshil Patel <harshilpatel1903@gmail.com>
2021-05-09 11:43:40 +0530

    1078. Occurrences After Bigram
    Source : https://leetcode.com/problems/occurrences-after-bigram/

--- HashTable/Ex_1078/occurences_after_bigram.py
+++ HashTable/Ex_1078/occurences_after_bigram.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution:
     def findOcurrences(self, text: str, first: str, second: str) -> List[str]:
         l = text.split()
@@ -10,3 +13,7 @@ class Solution:
                 res.append(l[i])
 
         return res
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.findOcurrences("alice is a good girl she is a good student", "a", "good"))
