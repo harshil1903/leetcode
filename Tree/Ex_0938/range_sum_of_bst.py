45763803d84256a27ad0bd99386497462ca3b0c5
Harshil Patel <harshilpatel1903@gmail.com>
2021-05-09 13:00:22 +0530

    938. Range Sum of BST
    Source : https://leetcode.com/problems/range-sum-of-bst/

---
+++ Tree/Ex_0938/range_sum_of_bst.py
@@ -0,0 +1,43 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+
+    #recursive
+    def rangeSumBST(self, root: TreeNode, low: int, high: int) -> int:
+
+        def helper(root):
+            if root:
+                if low <= root.val <= high:
+                    self.sum += root.val
+                if low < root.val:
+                    helper(root.left)
+                if high > root.val:
+                    helper(root.right)
+
+        self.sum = 0
+        helper(root)
+        return self.sum
+
+    def rangeSumBST1(self, root: TreeNode, low: int, high: int) -> int:
+        result = 0
+        s = [root]
+        while s:
+            node = s.pop()
+            if node:
+                if low <= node.val <= high:
+                    result += node.val
+                if low < node.val:
+                    s.append(node.left)
+                if node.val < high:
+                    s.append(node.right)
+        return result
+
+
+# Input: root = [10,5,15,3,7,null,18], low = 7, high = 15
+# Output: 32
