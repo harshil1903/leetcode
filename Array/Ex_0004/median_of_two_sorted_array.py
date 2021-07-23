520130527b1d5c741e6d901761c62d3d95e90d3e
Harshil Patel <harshilpatel1903@gmail.com>
2021-07-23 12:06:39 +0530

    Made changes to file format

--- Array/Ex_0004/median_of_two_sorted_array.py
+++ Array/Ex_0004/median_of_two_sorted_array.py
@@ -1,3 +1,12 @@
+
+# 4. Median of Two Sorted Arrays
+#
+# Source : https://leetcode.com/problems/median-of-two-sorted-arrays/
+#
+# Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+
+
 class Solution:
     def findMedianSortedArrays(self, nums1, nums2) -> float:
         size = len(nums1) + len(nums2)
