f96244aedf3066a0a079ce4fafa7c86113b1c9d6
Harshil Patel <harshilpatel1903@gmail.com>
2021-07-23 12:25:49 +0530

    Made changes to file format

--- Array/Ex_1480/running_sum_of_1d_array.py
+++ Array/Ex_1480/running_sum_of_1d_array.py
@@ -1,3 +1,14 @@
+
+# 1480. Running Sum of 1d Array
+#
+# Source : https://leetcode.com/problems/running-sum-of-1d-array/
+#
+# Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+#
+# Return the running sum of nums.
+
+
+
 class Solution:
     def runningSum(self, nums):
         temp = 0
--- Array/Ex_1512/number_of_good_pair.py
+++ Array/Ex_1512/number_of_good_pair.py
@@ -1,3 +1,16 @@
+
+# 1512. Number of Good Pairs
+#
+# Source : https://leetcode.com/problems/number-of-good-pairs/
+#
+# Given an array of integers nums.
+#
+# A pair (i,j) is called good if nums[i] == nums[j] and i < j.
+#
+# Return the number of good pairs.
+
+
+
 class Solution:
     def numIdenticalPairs(self, nums) -> int:
         k = 0
