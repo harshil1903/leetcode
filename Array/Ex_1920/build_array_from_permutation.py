f0d342cecde258d0c7219b962cc692e593193220
Harshil Patel <harshilpatel1903@gmail.com>
2021-07-23 10:37:53 +0530

    1920. Build Array from Permutation
    Source : https://leetcode.com/problems/build-array-from-permutation/

--- Array/Ex_1920/build_array_from_permutation.py
+++ Array/Ex_1920/build_array_from_permutation.py
@@ -12,8 +12,16 @@ class Solution:
 
         return res
 
+    def buildArray1(self, nums: List[int]) -> List[int]:
+        res = []
+
+        for i in nums:
+            res.append(nums[i])
+
+        return res
+
 
 if __name__ == '__main__':
     s = Solution();
     print(s.buildArray([0,2,1,5,3,4]))
-    print(s.buildArray([5,0,1,2,3,4]))
+    print(s.buildArray1([5,0,1,2,3,4]))
