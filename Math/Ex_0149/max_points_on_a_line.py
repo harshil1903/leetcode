479a2f0ff447223758a39d1c0ac965117e451757
Harshil Patel <harshilpatel1903@gmail.com>
2021-07-23 12:44:51 +0530

    Made changes to file format

--- Math/Ex_0149/max_points_on_a_line.py
+++ Math/Ex_0149/max_points_on_a_line.py
@@ -3,7 +3,8 @@
 #
 # Source : https://leetcode.com/problems/max-points-on-a-line/
 #
-# Given an array of points where points[i] = [xi, yi] represents a point on the X-Y plane, return the maximum number of points that lie on the same straight line.
+# Given an array of points where points[i] = [xi, yi] represents a point on the X-Y plane, return the maximum number of points that lie on the same straight
+# line.
 
 
 
