208302d9e1ae9d2f28893a648fa337ed9567bb92
Harshil Patel <harshilpatel1903@gmail.com>
2021-07-29 11:28:33 +0530

    1. Two Sum

--- Array/0001_two_sum.py
+++ Array/0001_two_sum.py
@@ -11,15 +11,31 @@
 
 
 
+from typing import List
+
 class Solution:
-    def twoSum(self, nums, target) -> list:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         container = {}
 
+        # enumerate(nums) gives i index and num as value at that index
         for i, num in enumerate(nums):
             if target - num in container:
                 return [container[target - num], i]
             container[num] = i
         return
+
+    def twoSum1(self, nums: List[int], target: int) -> List[int]:
+        dic = {}
+
+        for i in range(len(nums)):
+            x = target - nums[i]
+
+            # x is the second number needed along with nums[i] to make it equal to target
+            # if x is present in dic{} we have out result in form of i and index of x
+            if (x in dic):
+                return [dic[x], i]
+            else:
+                dic[nums[i]] = i  # if x is not in disc, save value and index of i for future values
 
 
 if __name__ == '__main__':
