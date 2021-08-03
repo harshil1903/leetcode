5980395c6ee3a17ad6b82098a9e1e401bbee47af
Harshil Patel <harshilpatel1903@gmail.com>
2021-08-03 11:22:15 +0530

    91. Decode Ways

---
+++ String/0091_decode_ways.py
@@ -0,0 +1,39 @@
+
+# 91. Decode Ways
+#
+# Source : https://leetcode.com/problems/decode-ways/
+#
+# A message containing letters from A-Z can be encoded into numbers using the following mapping:
+#
+# 'A' -> "1"
+# 'B' -> "2"
+# ...
+# 'Z' -> "26"
+# To decode an encoded message, all the digits must be grouped then mapped back into letters using the reverse of the mapping above (there may be multiple ways).
+# For example, "11106" can be mapped into:
+#
+# "AAJF" with the grouping (1 1 10 6)
+# "KJF" with the grouping (11 10 6)
+# Note that the grouping (1 11 06) is invalid because "06" cannot be mapped into 'F' since "6" is different from "06".
+#
+# Given a string s containing only digits, return the number of ways to decode it.
+#
+# The answer is guaranteed to fit in a 32-bit integer.
+
+
+
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        if len(s) == 0 or s[0] == '0':
+            return 0
+        prev, prev_prev = 1, 0
+        for i in range(len(s)):
+            cur = 0
+            if s[i] != '0':
+                cur = prev
+
+            # check if a pair of characters in between 10 and 26
+            if i > 0 and (10 <= int(s[i - 1:i + 1]) <= 26):
+                cur += prev_prev
+            prev, prev_prev = cur, prev
+        return prev
