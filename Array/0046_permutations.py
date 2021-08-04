9b09e73f221794baad0eedad3e8aca2ffde71380
Harshil Patel <harshilpatel1903@gmail.com>
2021-08-04 11:46:41 +0530

    46. Permutations

---
+++ Array/0046_permutations.py
@@ -0,0 +1,40 @@
+
+# 46. Permutations
+#
+# Source : https://leetcode.com/problems/permutations/
+#
+# Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+
+
+from typing import List
+
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        self.dfs(nums, [], res)
+        return res
+
+    def dfs(self, nums, path, res):
+        if not nums:
+            res.append(path)
+
+        for i in range(len(nums)):
+            self.dfs(nums[:i] + nums[i + 1:], path + [nums[i]], res)
+
+            # e.g., [1, 2, 3]: 3! = 6 cases
+            # idx -> nums, path
+            # 0 -> [2, 3], [1] -> 0: [3], [1, 2] -> [], [1, 2, 3]
+            #                  -> 1: [2], [1, 3] -> [], [1, 3, 2]
+            #
+            # 1 -> [1, 3], [2] -> 0: [3], [2, 1] -> [], [2, 1, 3]
+            #                  -> 1: [1], [2, 3] -> [], [2, 3, 1]
+            #
+            # 2 -> [1, 2], [3] -> 0: [2], [3, 1] -> [], [3, 1, 2]
+            #                  -> 1: [1], [3, 2] -> [], [3, 2, 1]
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.permute([1,2,3]));
